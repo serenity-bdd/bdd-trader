c28a0af1fd4dd01e68986fe26ffc043829eb824d
John Ferguson Smart <john.smart@wakaleo.com>
2018-06-06 08:32:27 +0100

    Removed redundant class

--- src/test/java/questions/RestQueryFunction.java
+++
@@ -1,7 +0,0 @@
-package questions;
-
-import io.restassured.specification.RequestSpecification;
-
-import java.util.function.Function;
-
-interface RestQueryFunction extends Function<RequestSpecification,RequestSpecification> {}
