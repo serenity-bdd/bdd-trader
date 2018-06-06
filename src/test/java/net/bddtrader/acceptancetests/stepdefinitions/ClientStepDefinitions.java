148c18e0346e7539e2eb395611b4ae7548162647
John Ferguson Smart <john.smart@wakaleo.com>
2018-06-06 14:26:49 +0100

    REfactored using tasks

--- src/test/java/net/bddtrader/acceptancetests/stepdefinitions/ClientStepDefinitions.java
+++ src/test/java/net/bddtrader/acceptancetests/stepdefinitions/ClientStepDefinitions.java
@@ -5,21 +5,18 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import net.bddtrader.acceptancetests.screenplay.questions.ThePortfolio;
+import net.bddtrader.acceptancetests.tasks.RegisterWithBDDTrader;
 import net.bddtrader.clients.Client;
-import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.abiities.CallAnApi;
-import net.serenitybdd.screenplay.rest.interactions.Post;
 import net.thucydides.core.util.EnvironmentVariables;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
 import java.util.Optional;
 
-import static net.bddtrader.acceptancetests.endpoints.BDDTraderEndPoints.RegisterClient;
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.rest.questions.ResponseConsequence.seeThatResponse;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
@@ -29,7 +26,6 @@ public class ClientStepDefinitions {
     private EnvironmentVariables environmentVariables;
     private Actor tim;
     private Client client;
-    private Optional<Client> registeredlient = Optional.empty();
 
     @Before
     public void configureBaseUrl() {
@@ -49,15 +45,8 @@ public class ClientStepDefinitions {
     @When("^the trader (?:attempts to register|registers) with BDD Trader$")
     public void the_trader_attempts_to_register_with_BDD_Trader() {
         tim.attemptsTo(
-                Post.to(RegisterClient.relativePath())
-                        .with(request -> request.header("Content-Type", "application/json")
-                                .body(client))
+                RegisterWithBDDTrader.asANewClient(client)
         );
-
-        if (SerenityRest.lastResponse().statusCode() == 200) {
-            this.registeredlient = Optional.of(SerenityRest.lastResponse().as(Client.class));
-        }
-
     }
 
     @Then("^the registration should be rejected with the message '(.*)'$")
@@ -72,10 +61,10 @@ public class ClientStepDefinitions {
     @Then("^the trader should have a portfolio with \\$(.*) in cash$")
     public void should_have_portfolio_with_cash(float expectedBalance) {
 
-        assertThat(registeredlient).isPresent();
+        Client registeredClient = tim.recall("registeredClient");
 
         tim.should(
-                seeThat(ThePortfolio.cashBalanceFor(registeredlient.get()), is(equalTo(expectedBalance)))
+                seeThat(ThePortfolio.cashBalanceFor(registeredClient), is(equalTo(expectedBalance)))
         );
     }
 }
