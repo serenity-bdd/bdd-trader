e5bfa26e67778c7986d3d32c925cffd1c0d04c5b
John Ferguson Smart <john.smart@wakaleo.com>
2018-06-07 19:50:58 +0300

    Trade history using tasks and questions

--- src/test/java/net/bddtrader/acceptancetests/stepdefinitions/TransactionHistoryStepDefinitions.java
+++ src/test/java/net/bddtrader/acceptancetests/stepdefinitions/TransactionHistoryStepDefinitions.java
@@ -1,21 +1,21 @@
 package net.bddtrader.acceptancetests.stepdefinitions;
 
 import cucumber.api.java.en.Then;
-import net.bddtrader.acceptancetests.endpoints.BDDTraderEndPoints;
 import net.bddtrader.acceptancetests.questions.ThePortfolio;
 import net.bddtrader.acceptancetests.tasks.FetchTransactionHistory;
 import net.bddtrader.clients.Client;
 import net.bddtrader.portfolios.Trade;
-import net.serenitybdd.rest.SerenityRest;
-import net.serenitybdd.screenplay.rest.interactions.Get;
-import org.hamcrest.Matchers;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 
+import java.util.Comparator;
 import java.util.List;
 
+import static net.bddtrader.acceptancetests.matchers.TradeMatchers.matchesTradesIn;
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 
 public class TransactionHistoryStepDefinitions {
 
@@ -28,18 +28,8 @@ public class TransactionHistoryStepDefinitions {
                 FetchTransactionHistory.forClient(registeredClient)
         );
 
-//        List<Trade> actualTransactionHistory = ThePortfolio.historyFor(registeredClient).answeredBy(theActorInTheSpotlight());
-
         theActorInTheSpotlight().should(
-                seeThat(ThePortfolio.history(), equalTo(transactionHistory))
-
+                seeThat(ThePortfolio.history(), matchesTradesIn(transactionHistory))
         );
-//        List<Trade> actualTransactionHistory = SerenityRest.lastResponse()
-//                                                           .jsonPath()
-//                                                           .getList("history", Trade.class);
-//
-//        assertThat(actualTransactionHistory).usingElementComparatorIgnoringFields("id","timestamp")
-//                                            .containsExactlyElementsOf(transactionHistory);
     }
-
 }
