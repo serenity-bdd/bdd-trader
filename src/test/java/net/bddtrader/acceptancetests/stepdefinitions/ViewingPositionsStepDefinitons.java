c05b6ad9d0cf0fd4d4fb5647ce1eb7fc19644b2b
John Ferguson Smart <john.smart@wakaleo.com>
2018-06-07 17:30:03 +0300

    Exercise 1 solution

--- src/test/java/net/bddtrader/acceptancetests/stepdefinitions/ViewingPositionsStepDefinitons.java
+++ src/test/java/net/bddtrader/acceptancetests/stepdefinitions/ViewingPositionsStepDefinitons.java
@@ -1,16 +1,21 @@
 package net.bddtrader.acceptancetests.stepdefinitions;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import net.bddtrader.acceptancetests.endpoints.BDDTraderEndPoints;
 import net.bddtrader.acceptancetests.model.MarketPrice;
+import net.bddtrader.acceptancetests.questions.ThePortfolio;
 import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.rest.interactions.Get;
 import net.serenitybdd.screenplay.rest.interactions.Post;
 
 import java.util.List;
 import java.util.Map;
 
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ViewingPositionsStepDefinitons {
@@ -30,5 +35,21 @@ public class ViewingPositionsStepDefinitons {
                                              .body(marketPrice.getPrice()))
         );
         assertThat(SerenityRest.lastResponse().statusCode()).isEqualTo(200);
+    }
+
+    @And("^the overall profit should be \\$(.*)$")
+    public void theOverallProfitShouldBe(double expectedProfit) throws Throwable {
+
+        Integer portfolioId = theActorInTheSpotlight().recall("clientPortfolioId");
+
+        theActorInTheSpotlight().attemptsTo(
+                Get.resource(BDDTraderEndPoints.PortfolioProfit.path())
+                   .with(request -> request.pathParam("portfolioId", portfolioId)
+                                           .header("Content-Type", "application/json"))
+        );
+
+        Double actualProfit = SerenityRest.lastResponse().as(Double.class);
+
+        assertThat(actualProfit).isEqualTo(expectedProfit);
     }
 }
